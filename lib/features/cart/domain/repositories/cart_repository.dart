9e73d95f0bcf3b4583d814f223c594f53fc535a2
vanstrong4 <alfeushendrievan@gmail.com>
2026-03-04 14:38:22 +0700

    bikin kontrak function

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -1,3 +1,8 @@
-abstract class CartRepository {
+import 'package:food_catalog/features/cart/domain/entities/product.dart';
 
+abstract class CartRepository {
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAll();
+  bool isItemInCart(String productId);
 }
