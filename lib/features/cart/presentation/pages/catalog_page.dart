f8e8fdf4ac5e1eb53c7ee80e6d274ff399a558b5
vanstrong4 <alfeushendrievan@gmail.com>
2026-03-04 14:53:01 +0700

    bikin tapilan buat catalog

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/core/constants/app_constants.dart';
+import 'package:food_catalog/core/routes/app_router.dart';
 import 'package:food_catalog/features/cart/domain/entities/product.dart';
+import 'package:food_catalog/features/cart/presentation/widgets/add_button_widget.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
@@ -12,6 +15,30 @@ class CatalogPage extends StatelessWidget {
       Product(id: '3', name: 'Sprite', price: '12000'),
       Product(id: '4', name: 'Big Cola', price: '10000'),
     ];
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: const Text(AppConstants.catalogTitle, style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, color: Colors.white,),
+            onPressed: () => Navigator.pushNamed(context, AppRouter.cart),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            trailing: AddButtonWidget(product: products[index]),
+          );
+        },
+      ),
+    );
   }
 }
