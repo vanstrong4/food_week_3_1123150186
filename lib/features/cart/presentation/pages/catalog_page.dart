9a08d39a512a2e263e58395449e1f23be7a96bf9
vanstrong4 <alfeushendrievan@gmail.com>
2026-03-04 14:49:09 +0700

    tambahin minuman

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/features/cart/domain/entities/product.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final products = [
+      Product(id: '1', name: 'Coca Cola', price: '12000'),
+      Product(id: '2', name: 'Fanta Strawberry', price: '12000'),
+      Product(id: '3', name: 'Sprite', price: '12000'),
+      Product(id: '4', name: 'Big Cola', price: '10000'),
+    ];
     return const Placeholder();
   }
 }
