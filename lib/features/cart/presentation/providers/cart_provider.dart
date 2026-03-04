046a4d18bdd9032a0ab4e6d24332c888cbd81542
vanstrong4 <alfeushendrievan@gmail.com>
2026-03-04 14:42:52 +0700

    bikin notify listeners provider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/features/cart/domain/entities/product.dart';
+import 'package:food_catalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartProvider extends ChangeNotifier {
-  
+  final CartRepository _repository;
+
+  CartProvider({required CartRepository repository}) : _repository = repository;
+
+  List<Product> get items => _repository.getCartItems();
+
+  void addItem(Product product) {
+    _repository.addItem(product);
+    notifyListeners();
+  }
+
+  void removeAll(){
+    _repository.removeAll();
+    notifyListeners();
+  }
+
+  bool isItemInCart(String productId) => _repository.isItemInCart(productId);
 } 
