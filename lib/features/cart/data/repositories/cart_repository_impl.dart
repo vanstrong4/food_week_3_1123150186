7143b031cff40dcbd4680526a62984383cb8e5a8
vanstrong4 <alfeushendrievan@gmail.com>
2026-03-04 14:39:55 +0700

    mengoverride function yang ada di CartRepository

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -1,5 +1,18 @@
+import 'package:food_catalog/features/cart/domain/entities/product.dart';
 import 'package:food_catalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartRepositoryImpl implements CartRepository {
+  final List<Product> _items = [];
 
+  @override
+  List<Product> getCartItems() => List.unmodifiable(_items);
+
+  @override
+  void addItem(Product product) => _items.add(product);
+
+  @override
+  void removeAll() => _items.clear();
+
+  @override
+  bool isItemInCart(String productId) => _items.any((p) => p.id == productId);
 } 
